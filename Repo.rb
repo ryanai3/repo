4443ffd09394b0bf588b095163289b1c7ec79f14
Ryan Benmalek <ryanai3@hotmail.com>
2015-06-26 11:31:37 -0700

    check if repofile exists

--- Repo.rb
+++ Repo.rb
@@ -1,6 +1,16 @@
+#!/usr/bin/env ruby
 require 'rugged'
 require 'thor'
-current_directory = Dir.pwd
 
 
+class Repo < Thor
+  desc "add", "Add file contents to the index"
+  def add
+    current_directory = Dir.pwd
+    repo_file = File.expand_path("/.repo", Dir.pwd)
+    print(File.exist?(repo_file))
+  end
 
+end
+
+Repo.start
