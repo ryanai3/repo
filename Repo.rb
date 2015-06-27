091426d2d0461ff2bd534c6de0977d730f6eba51
Ryan Benmalek <ryanai3@hotmail.com>
2015-06-26 22:48:01 -0700

    filled out stage_files - Repo can now stage files in it's .repo, working on staging file in subrepos

--- Repo.rb
+++ Repo.rb
@@ -20,7 +20,8 @@ class Repo < Thor
     repofile2gitfile
     if args.all?{|i| refers_to_file?(i)}
       #if add is used simply, (listing files) we can handle it ourself
-
+      index = Rugged::Index.new(@current_dir)
+      
 
     else # if args, use git (for interactive stuff too) and then repoify it
       system_call_git(*args)
@@ -29,6 +30,23 @@ class Repo < Thor
 
 
     print("success")
+  end
+
+  def stage_files(files)
+    #filter files - ones in current dir added to index, rest handed off to subrepos
+    files_in_current_dir = files.select{ |file| File.dirname(file) == @current_dir}
+    files_to_handoff = files - files_in_current_dir #set difference
+    #getindex
+    cur_index = Rugged::Index.new(@current_dir)
+    #stage files in current directory
+    files_in_current_dir.each{ |file| cur_index << File.path(file)}
+
+    files_to_handoff.group_by{ |file| File.dirname(file)}.each{ |dir, files| stage_files_in(dir, files)}
+  end
+  #
+  def stage_files_in(dir, files)
+    #if dir contains .repo, create a Repo on there and call stage on it
+    #else, stage them all yourself.
   end
 
   desc "git", "turns .repo unto .git then calls git"
